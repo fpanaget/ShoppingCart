777ec80182d64bdeb98b1e80a4048b970c645c3a
lara.ziosi <lara.ziosi@nl.ibm.com>
2017-12-24 21:46:53 +0100

    added select of products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { OrderComponent } from './order/order.component';
 import { UserComponent } from './user/user.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -13,10 +16,13 @@ import { UserComponent } from './user/user.component';
     AppComponent,
     ProductComponent,
     OrderComponent,
-    UserComponent
+    UserComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {PRODUCTS} from '../domain/mock-products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products = PRODUCTS;
+  selectedProduct : Product=PRODUCTS[0];
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+  onSelect( product : Product):void{
+    this.selectedProduct = product;
+  }
+
+}
