567a614a6c70dc980612bf0ebf25a8e994397eab
lara.ziosi <lara.ziosi@nl.ibm.com>
2018-01-14 12:32:30 +0100

    added properties to Product

--- src/app/domain/product.ts
+++ src/app/domain/product.ts
@@ -4,6 +4,17 @@ export class Product{
   name: string,
   description: string,
   price: number
-){}
+){
+this.id = id;
+this.name = name;
+this.description= description;
+this.price = price;
+
+}
+id:number;
+name:string;
+description: string;
+price:number;
+
 
 }
